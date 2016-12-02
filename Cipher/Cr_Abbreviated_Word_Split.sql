8a8da99fa59a239b8de209dbe7d9fb3688ff4464
a-difranco-yahoo-com <a.difranco@yahoo.com>
2016-12-02 11:31:12 +0000

    Added I'M / YOUR'N

--- Cipher/Cr_Abbreviated_Word_Split.sql
+++ Cipher/Cr_Abbreviated_Word_Split.sql
@@ -13,3 +13,23 @@ INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('THER
 INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('I''D',     'I HAD');
 INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('HE''D',    'HE HAD');
 INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('YOU''D',   'YOU HAD');
+--
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('AIN''T',     'AM NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('WEREN''T',   'WERE NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('CAN''T',     'CAN NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('MAYN''T',    'MAY NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('SHOULDN''T', 'SHOULD NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('WARN''T',    'WAS NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('DON''T',     'DO NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('WOULDN''T',  'WOULD NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('DIDN''T',    'DID NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('WASN''T',    'WAS NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('WON''T',     'WILL NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('HADN''T',    'HAD NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('COULDN''T',  'COULD NOT');
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('DOESN''T',   'DOES NOT');
+--
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('I''M',     'I AM');
+--
+INSERT INTO ABBREVIATED_WORD_SPLIT (Abbreviated_Word, Split_Words) Values ('YOUR''N',   'YOURS');
+
