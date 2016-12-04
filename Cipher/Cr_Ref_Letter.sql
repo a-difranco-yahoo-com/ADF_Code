c12ea7fd51702d6f9a545f2cbbddba2304a34283
a-difranco-yahoo-com <a.difranco@yahoo.com>
2016-12-04 09:47:34 +0000

    Initial Commit

---
+++ Cipher/Cr_Ref_Letter.sql
@@ -0,0 +1,11 @@
+
+Drop   Table  REF_LETTER Purge
+/
+
+Create Table  REF_LETTER (
+Letter       Varchar2(1),
+Frequency    Number,
+Starts_With  Number,
+End_With     Number)
+/
+
---
+++ Cipher/Cr_Ref_Trigraph.sql
@@ -0,0 +1,11 @@
+
+Drop   Table  REF_TRIGRAPH Purge
+/
+
+Create Table  REF_TRIGRAPH (
+Word         Varchar2(3),
+Frequency    Number,
+Starts_With  Number,
+End_With     Number)
+/
+
