0a88b24754e3de176793a6502f43d9b366497c28
a-difranco-yahoo-com <a.difranco@yahoo.com>
2018-07-08 11:57:36 +0100

    Latest local version

--- SqlDeveloper/ADF.sql
+++ SqlDeveloper/ADF.sql
@@ -1,8 +1,11 @@
+SELECT * FROM DBA_DIRECTORIES;
 
+Create Or Replace Directory EXPDP_DIR as 'E:\My Dropbox\Dropbox\MyBackup';
 
 SELECT Time_Remaining, Message FROM V$Session_Longops WHERE Time_Remaining > 0;
-SELECT * FROM V$SEssion;
-
+SELECT * FROM V$SEssion WHERE Username IS NOT NULL;
+ALTER SYSTEM KILL SESSION '371,	51460'; 
+SELECT * FROM V$SQLAREA;
 
 SELECT * FROM GEOCACHE_BY_DAY;
 
@@ -10,7 +13,7 @@ SELECT sum(Caches)     FROM GEOCACHE_BY_DAY   ;
 SELECT sum(Days_Found) FROM V_GEOCACHE_TO_FIND_BY_MONTH   ;
 
 SELECT * FROM    V_GEOCACHE_BY_MONTH;
-SELECT * FROM    V_GEOCACHE_BY_DAY_OF_MONTH;
+SELECT * FROM    V_GEOCACHE_BY_DAY_OF_MONTH Order By  2 Desc;
 SELECT * FROM    V_GEOCACHE_TO_FIND_BY_MONTH;
 
 
@@ -34,7 +37,7 @@ END;
 SELECT Owner, Segment_Name, Bytes / (1024 * 1024) Mb
 FROM   DBA_SEGMENTS
 WHERE  Owner != 'SYS'
-AND    Segment_Name Like '%SFA%'
+AND    Segment_Name Like '%QUAG%'
 ORDER BY Bytes Desc
 /
 
@@ -44,60 +47,18 @@ GROUP BY Tablespace_Name
 /
 
 
-SELECT Sysdate + (Time_Remaining/ (60 * 60 * 24) ) ETA, Time_Remaining, Message FROM V$Session_Longops WHERE Time_Remaining > 0 order by 1;
-SELECT S.Username, T.SQL_Text
-FROM   V$Session S
-  JOIN V$Sqlarea T ON T.Sql_Id = S.Sql_Id;
-
-
---interpret vital telexes
--- nrpr
-
-Drop   Table GC_IVT Purge;
-Create Table GC_IVT
-AS
-SELECT   Distinct Word, length(Word) Word_Length
-FROM     DICTIONARY.Word_List
-WHERE    length(Word) IN (5, 7, 9)
-AND      translate(Word, 'xINTERPRETVITALTELEXES', 'x') IS NULL
-/
-
-Drop   Table GC_IVT_ANSWER Purge
-/
-Create Table GC_IVT_ANSWER
-AS
-SELECT *
-FROM  (
-SELECT L7.Word W7, L5.Word W5, L9.Word W9, L7.Word || L5.Word || L9.Word Word 
-FROM   GC_IVT L9
-  JOIN GC_IVT L5 ON L5.Word_Length = 5
-  JOIN GC_IVT L7 ON L7.Word_Length = 7
-WHERE  L9.Word_Length = 9
-)
-WHERE  Word Like '%X%'
-AND    Word Like '%V%'
-AND    Word Like '%P%'
-AND    Word Like '%L%L%'
-AND    Word Like '%N%'
-AND    Word Like '%A%'
-AND    Word Like '%S%'
-AND    Word Like '%I%I%'
-AND    Word Like '%R%R%'
-AND    Word Like '%T%T%T%T%'
-AND    Word Like '%E%E%E%E%E%'
-/
-Select * FROM GC_IVT_ANSWER
-Order By W7
-/
 
 SELECT Word
 FROM   DICTIONARY.Word_List
 WHERE  length(Word) = 8
-AND    Word Like '%B%'
+AND    Word Like '%T%'
+AND    Word Like '%A%'
 AND    Word Like '%K%'
-AND    Word Like '%P%'
-AND    Word Like '%S%'
-AND    Word Like '%O%O%O%'
+AND    Word Like '%E%'
+AND    Word Like '%N%'
+AND    Word Like '%B%'
+AND    Word Like '%U%'
+AND    Word Like '%M%'
 ;
 
 drop table DARTS_PL Purge;
@@ -140,6 +101,7 @@ ORDER BY Ws - Ds Desc
 /
 
 SELECT Year, Player,
+       Count(1) Played,
        sum(CASE WHEN F > A THEN 1 ELSE 0 END) W,
        sum(CASE WHEN F = A THEN 1 ELSE 0 END) D,
        sum(CASE WHEN F < A THEN 1 ELSE 0 END) L,
@@ -151,6 +113,7 @@ FROM  (
         UNION ALL
         SELECT Year, Player_A Player, Score_A F, Score_H A  FROM   DARTS_PL WHERE (Score_H + Score_A) > 0 AND Round Not Like '%F'
        ) 
+WHERE    Year = 2018
 GROUP BY Year, Player
 ORDER BY Year, Pts Desc, F - A Desc
 /
@@ -161,4 +124,54 @@ WHERE  ROUND = 'F'
 ORDER BY Year
 /
 
+SELECT * FROM   ALL_TABLES;
+SELECT * FROM   ALL_USERS Where User_ID between 112 AND 1000 Order By Created Desc;
 
+SELECT count(*)
+FROM  (
+WITH StartPoint AS (SELECT Rownum X FROM ALL_OBJECTS WHERE Rownum <= 15)
+SELECT *
+FROM   StartPoint S1
+  JOIN StartPoint S2  ON (2  > 5 AND S2.X  = 15) OR (2  <= 5 AND S2.X  > S1.X)
+  JOIN StartPoint S3  ON (3  > 5 AND S3.X  = 15) OR (3  <= 5 AND S3.X  > S2.X)
+  JOIN StartPoint S4  ON (4  > 5 AND S4.X  = 15) OR (4  <= 5 AND S4.X  > S3.X)
+  JOIN StartPoint S5  ON (5  > 5 AND S5.X  = 15) OR (5  <= 5 AND S5.X  > S4.X)
+  JOIN StartPoint S6  ON (6  > 5 AND S6.X  = 15) OR (6  <= 5 AND S6.X  > S5.X)
+  JOIN StartPoint S7  ON (7  > 5 AND S7.X  = 15) OR (7  <= 5 AND S7.X  > S6.X)
+  JOIN StartPoint S8  ON (8  > 5 AND S8.X  = 15) OR (8  <= 5 AND S8.X  > S7.X)
+  JOIN StartPoint S9  ON (9  > 5 AND S9.X  = 15) OR (9  <= 5 AND S9.X  > S8.X)
+  JOIN StartPoint S10 ON (10 > 5 AND S10.X = 15) OR (10 <= 5 AND S10.X > S9.X)
+  JOIN StartPoint S11 ON (11 > 5 AND S11.X = 15) OR (11 <= 5 AND S11.X > S10.X)
+  JOIN StartPoint S12 ON (12 > 5 AND S12.X = 15) OR (12 <= 5 AND S12.X > S11.X)
+  JOIN StartPoint S13 ON (13 > 5 AND S13.X = 15) OR (13 <= 5 AND S13.X > S12.X)
+  JOIN StartPoint S14 ON (14 > 5 AND S14.X = 15) OR (14 <= 5 AND S14.X > S13.X)
+  JOIN StartPoint S15 ON (15 > 5 AND S15.X = 15) OR (15 <= 5 AND S15.X > S14.X)
+)
+/
+
+SELECT * FROM V$SESSION WHERE Username Is Not NULL;
+ALTER SYSTEM KILL SESSION '244,	14850';
+
+SELECT Sysdate + (Time_Remaining/ (60 * 60 * 24) ) ETA, Time_Remaining, username, Message 
+FROM V$Session_Longops 
+WHERE Time_Remaining > 0 
+order by 1;
+SELECT S.Username, S.SQL_ID, T.SQL_Text
+FROM   V$Session S
+  JOIN V$Sqlarea T ON T.Sql_Id = S.Sql_Id
+/
+
+SELECT
+  (1000 - 960) -- Paddington Dare (W London)
++ (1000 - 850) -- 16th Century Pub (Central London)
++ (1000 - 672) -- Reflections of Neal's Yard
++ (1000 - 606) -- The Palace of Westminster (Houses of Parliament)
++ (1000 - 509) -- The Winchester Geese
++ (1000 - 473) -- Last Delivery (London)
++ (1000 - 416) -- Under the boardwalk (SE1, London)
+-- + (1000 - 396) -- Catastrophe, Calamity, Cataclysm Part 3
+-- + (1000 - 392) -- Gibbon's Rent
+       Required
+FROM DUAL
+/
+-- 2514
--- SqlDeveloper/Digital_Comics.sql
+++ SqlDeveloper/Digital_Comics.sql
@@ -41,17 +41,18 @@ BEGIN
 END;
 /
 
+DEFINE Text="%Mon%Unl%%%"
+SELECT * FROM V_DIGITAL_RUN_DETAIL      WHERE Title Like '%&&Text%' Order By Volume,Title;
+SELECT * FROM V_DIGITAL_WISH_RUN_DETAIL WHERE Title Like '%&&Text%' Order By Title;
+
 SELECT * FROM V_DIGITAL_MULTI_RUN_DETAIL;
-SELECT * FROM V_DIGITAL_ALL_MULTI_RUN_DETAIL;
+SELECT * FROM V_DIGITAL_ALL_MULTI_RUN_DETAIL;            
 
 
 BEGIN
-   COMICS.Load_Wish_List('Sub-Mariner', 1968, 15, 21);
+   COMICS.Load_Wish_List('Adventures of Superman Annual',	1987,	6,	6);
 END;
 /
-
-SELECT * FROM WISH_LIST WHERE Title Like '%Mis%Mir%' AND Volume > 1007 ORDER BY Volume, Issue;
-COMMIT;
 
 
 SELECT   Title, Volume, Count(*) Quantity
@@ -59,8 +60,101 @@ FROM     WISH_LIST
 GROUP BY Title, Volume
 ORDER BY Count(*) Desc;
 
-SELECT   to_char(Last_Updated, 'MON-YYYY') Month, Count(*)
+SELECT   to_char(Last_Updated, 'YYYYMM-MON') Month, Count(*)
 FROM     SUBSCRIPTION
-GROUP BY to_char(Last_Updated, 'MON-YYYY')
+GROUP BY to_char(Last_Updated, 'YYYYMM-MON')
 ORDER BY 2
 /
+
+
+SELECT L.Title, H.Title, L.Volume, L.Start_Issue, L.End_Issue, L.Series_Run, H.Start_Issue, H.End_Issue, H.Series_Run
+FROM   V_DIGITAL_RUN_DETAIL L
+  JOIN V_DIGITAL_RUN_DETAIL H ON H.Volume = L.Volume
+                             AND H.Start_Issue = L.End_Issue + 1
+WHERE  SYS.utl_match.jaro_winkler_similarity(upper(L.Title),  upper(H.Title)) between 75 and 85
+AND    coalesce(H.SubIssue, L.SubIssue) IS NULL
+AND    L.Title > 'The She'
+--AND    H.Title Like '%B.P.%'
+ORDER BY L.Title
+/
+
+
+DROP   TABLE LIST_HIERARCHY PURGE;
+CREATE TABLE LIST_HIERARCHY
+AS
+SELECT
+      CASE WHEN List1_First > List2_First THEN List1 ELSE List2 END First_List,
+      CASE WHEN List1_First > List2_First THEN List2 ELSE List1 END Second_List
+FROM  (      
+SELECT List1, List2, count(*),
+       SUM(CASE WHEN Min_List1_Issue < Min_List2_Issue THEN 1 ELSE 0 END) List1_First,
+       SUM(CASE WHEN Min_List1_Issue > Min_List2_Issue THEN 1 ELSE 0 END) List2_First
+FROM  (
+        SELECT T1.List List1, T2.List List2, T1.Title,
+               min( (T1.Volume * 1000000) + T1.Issue) Min_List1_Issue,
+               min( (T2.Volume * 1000000) + T2.Issue) Min_List2_Issue
+        FROM   MERGED_READING_ORDER T1
+          JOIN MERGED_READING_ORDER T2 ON T2.Title = T1.Title
+                                      AND T2.List  > T1.List
+        GROUP BY T1.List, T2.List, T1.Title
+)
+GROUP BY List1, List2
+)
+ORDER BY 1, 2
+/
+
+UPDATE LIST_ORDER L
+SET    Reading_Order = Reading_Order + 1
+WHERE EXISTS (SELECT 1
+              FROM    LIST_HIERARCHY H
+                JOIN  LIST_ORDER     B ON B.List = H.First_List
+              WHERE   H.Second_List = L.List
+              AND     B.Reading_Order >= L.Reading_Order)
+/
+
+SELECT *
+FROM   LIST_ORDER
+ORDER By 2, 1
+/
+CREATE TABLE DELETE_LIST_HIERARCHY
+AS
+SELECT First_List, Second_List
+FROM   LIST_HIERARCHY
+INTERSECT
+SELECT L1.First_List, L2.Second_List
+FROM   LIST_HIERARCHY L1
+  JOIN LIST_HIERARCHY L2 ON L2.First_List = L1.SECOND_LIST
+/
+DELETE
+FROM   LIST_HIERARCHY
+WHERE (First_List, Second_List) IN
+           (SELECT L1.First_List, L2.Second_List
+            FROM   LIST_HIERARCHY L1
+              JOIN LIST_HIERARCHY L2 ON L2.First_List = L1.SECOND_LIST)
+/
+
+INSERT INTO List_Hierarchy
+SELECT Distinct List, List
+FROM   MERGED_READING_ORDER
+WHERE  List NOT IN (SELECT Second_List FROM List_Hierarchy)
+/
+
+SELECT List, count(*) 
+FROM   MERGED_READING_ORDER
+GROUP BY List
+ORDER BY 2
+/
+
+SELECT L.*, Level
+FROM   LIST_HIERARCHY L
+CONNECT BY PRIOR Second_List = First_List AND Second_List != First_List
+START WITH First_List = Second_List
+ORDER BY Level
+/
+
+
+SELECT *
+FROM   merged_reading_order
+WHERE  List IN ('Inhumanity', 'Avengers_Standoff')
+AND    Title = 'New Avengers'
+
