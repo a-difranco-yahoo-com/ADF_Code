11d8f20803e1e4b4dc1cdc0c187c370fbd913225
a-difranco-yahoo-com <a.difranco@yahoo.com>
2018-07-15 21:53:49 +0100

    ADd to GIT

---
+++ Crossword/Cr_Crossword_Clue.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE CROSSWORD_CLUE (
+Clue_No    NUMBER      NOT NULL,
+AorD       VARCHAR2(1) NOT NULL,
+Row_No     NUMBER      NOT NULL,
+Column_No  NUMBER      NOT NULL)
+/
---
+++ Crossword/Cr_Crossword_Key_Cell.sql
@@ -0,0 +1,7 @@
+
+
+CREATE TABLE CROSSWORD_KEY_CELL (
+Key_No     NUMBER      NOT NULL,
+Row_No     NUMBER      NOT NULL,
+Column_No  NUMBER      NOT NULL)
+/
