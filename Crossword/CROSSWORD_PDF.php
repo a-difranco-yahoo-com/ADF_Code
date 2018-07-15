ec648553d40d7ef33a76f66fc39677f73988acaa
a-difranco-yahoo-com <a.difranco@yahoo.com>
2018-07-15 21:57:28 +0100

    Added Key Cell support

--- Crossword/CROSSWORD_PDF.php
+++ Crossword/CROSSWORD_PDF.php
@@ -28,21 +28,49 @@ class CROSSWORD_PDF extends FPDF
    }
 
 
-   public function PrintCell($Row, $Column, $Entry)
+   public function PrintClueNumber($Row, $Column, $ClueNumber)
+   {
+      $this->SetFont('Arial','B',10);
+//
+      $this->SetXY( ($Column - 0.5) * $this->Space, ($Row - 0.5) * $this->Space);
+	  $this->Write(4, $ClueNumber);
+   }
+
+   public function PrintCell($Row, $Column, $Entry, $Is_Key_Cell)
    {
       $this->SetLineWidth(0.1);
       $this->SetDrawColor(0,0,0);
+      $this->SetFont('Arial','B',16);
+	  if     ($Entry       == "." ) $this->SetFillColor(  0,   0,   0);
+	  elseif ($Is_Key_Cell == "Y" ) $this->SetFillColor(  0, 255,   0);
+	  else                          $this->SetFillColor(255, 255, 255);		  
 //
       $this->SetXY( ($Column - 0.5) * $this->Space, ($Row - 0.5) * $this->Space);
-	  if ( $Entry == "." )
+	  if ( $Entry == "." || $Entry == "x" )
 	  {
-		  $this->Cell($this->Space, $this->Space, "", "LRTB", 0, "C", TRUE);
-	  } elseif ( $Entry == "x" )
-	  {
-		  $this->Cell($this->Space, $this->Space, "", "LRTB", 0, "C", FALSE);
+		  $this->Cell($this->Space, $this->Space, "",     "LRTB", 0, "C", TRUE);
 	  } else {
-		  $this->Cell($this->Space, $this->Space, $Entry, "LRTB", 0, "C", FALSE);
+		  $this->Cell($this->Space, $this->Space, $Entry, "LRTB", 0, "C", TRUE);
 	  }
+   }
+
+   public function SetupKeyCellPage()
+   {
+      $this->AddPage();
+      $this->SetDrawColor(0,0,0);
+      $this->SetFont('Arial','B',8);
+      $this->SetMargins(15, 15, 15);
+      $this->Ln();
+   }
+
+   public function PrintKeyCell($Key_No, $Row, $Column, $Entry, $Value)
+   {
+      $this->Cell(36, 8, $Key_No, "LRTB", 0, "C", FALSE);
+      $this->Cell(36, 8, $Row,    "LRTB", 0, "C", FALSE);
+      $this->Cell(36, 8, $Column, "LRTB", 0, "C", FALSE);
+      $this->Cell(36, 8, $Entry,  "LRTB", 0, "C", FALSE);
+      $this->Cell(36, 8, $Value,  "LRTB", 0, "C", FALSE);
+      $this->Ln();
    }
 
 
