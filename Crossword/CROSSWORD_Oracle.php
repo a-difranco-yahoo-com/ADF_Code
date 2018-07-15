da411aa4571d22dfdd9293e746cb0002fb82007f
a-difranco-yahoo-com <a.difranco@yahoo.com>
2018-07-15 21:56:14 +0100

    Added Key Cell support

--- Crossword/CROSSWORD_Oracle.php
+++ Crossword/CROSSWORD_Oracle.php
@@ -32,8 +32,34 @@ class CROSSWORD_Oracle
 
    public function Get_Cells()
    {
-       $SelectText = "SELECT Row_No, Column_No,  Entry";
-       $FromText    = "FROM   CROSSWORD_CELL";
+       $SelectText = "SELECT Row_No, Column_No,  Entry, Is_Key_Cell";
+       $FromText   = "FROM   V_CROSSWORD_CELL";
+       $stmt = oci_parse($this->Connection, "$SelectText $FromText");
+       oci_execute($stmt, OCI_DEFAULT);
+
+       $rows = oci_fetch_all($stmt, $results, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+       oci_free_statement($stmt);
+       return $results;
+   }
+
+   public function Get_Key_Cells()
+   {
+       $SelectText = "SELECT   Key_No, Row_No, Column_No, Entry, Lookup_Value";
+       $FromText   = "FROM     V_CROSSWORD_CELL";
+       $WhereText  = "WHERE    Key_No IS NOT NULL";
+       $OrderText  = "ORDER BY Key_No";
+       $stmt = oci_parse($this->Connection, "$SelectText $FromText $WhereText $OrderText");
+       oci_execute($stmt, OCI_DEFAULT);
+
+       $rows = oci_fetch_all($stmt, $results, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+       oci_free_statement($stmt);
+       return $results;
+   }
+
+   public function Get_Clues()
+   {
+       $SelectText = "SELECT  Row_No, Column_No,  Clue_No";
+       $FromText    = "FROM   CROSSWORD_CLUE";
        $stmt = oci_parse($this->Connection, "$SelectText $FromText");
        oci_execute($stmt, OCI_DEFAULT);
 
