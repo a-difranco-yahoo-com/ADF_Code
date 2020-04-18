7eb842d6e18a4f5c25f16441b5d4d83f0fa658e5
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-04-18 12:50:42 +0100

    Create Cr_Placement.sql

---
+++ Jigsaw/Cr_Placement.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE PLACEMENT (
+Placement_Id    NUMBER      NOT NULL,
+Shape_Id        NUMBER      NOT NULL,
+Square_Id       NUMBER      NOT NULL,
+Letter          VARCHAR2(1) NOT NULL)
+/
