431bc67966648dc4f3842891215c2256d568211c
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-04-18 13:01:20 +0100

    Update Quagg_Yo_Mama.sql

--- Geocache_Solvers/Solved/Quagg_Yo_Mama.sql
+++ Geocache_Solvers/Solved/Quagg_Yo_Mama.sql
@@ -36,9 +36,10 @@ AND    Word_Id2     IN (SELECT Word_Id FROM QUAGMIRE_DIAGNOSTIC_RUN WHERE Solved
 ORDER By MATCHED_LETTERS Desc, MISMATCHED_LETTERS Desc
 /
 
-SELECT * FROM QUAGMIRE_DIAGNOSTIC_RUN  ;
+SELECT * FROM QUAGMIRE_DIAGNOSTIC_RUN ORDER BY Word_Id ;
 SELECT * FROM QUAGMIRE_DIAGNOSTIC_MISMATCH;
 SELECT * FROM QUAGMIRE_SUGGESTED_WORD ;
+SELECT * FROM QUAGMIRE_SOLUTION ;
 
 SELECT Mismatch_Word_Id, count(distinct Attempted_Word_Id) FROM QUAGMIRE_DIAGNOSTIC_MISMATCH GROUP BY Mismatch_Word_Id Order By 2 Desc;
 SELECT count(*),  Solved  FROM QUAGMIRE_DIAGNOSTIC_RUN  GROUP BY Solved  ORDER BY Solved;
@@ -88,5 +89,51 @@ WHERE  Offset = 0
 ORDER BY Letter
 /  
 
+-SELECT *
+FROM   USER_INDEXES
+WHERE  Table_Name Like 'QUAGMIRE_DICTIONARY%'
+/
+
+INSERT INTO QUAGMIRE_DIAGRAPH (Word_Id1, Word_Id2, Dictionary_Id1, Dictionary_Id2) 
+/
+SELECT DW1.Word_Id, DW2.Word_Id, DW1.Dictionary_Id, DW2.Dictionary_Id 
+FROM   QUAGMIRE_DICTIONARY_WORD DW1   
+  JOIN QUAGMIRE_DICTIONARY      D1  ON  D1.Dictionary_Id = DW1.Dictionary_Id
+  JOIN QUAGMIRE_DICTIONARY_WORD DW2 ON DW2.Word_Id       = 40
+                                   AND DW2.Valid         = 'Y'
+  JOIN QUAGMIRE_DICTIONARY      D2  ON  D2.Dictionary_Id = DW2.Dictionary_Id
+WHERE  DW1.Word_Id = 100
+AND    DW1.Valid   = 'Y'
+AND    substr(D1.Word, 3, 1)  != substr(D2.Word, 1, 1)
+AND    substr(D1.Word, 9, 1)   = substr(D2.Word, 1, 1)
+AND    substr(D1.Word, 4, 1)  != substr(D2.Word, 2, 1)
+AND    substr(D1.Word, 10, 1) != substr(D2.Word, 2, 1)
+AND    substr(D1.Word, 5, 1)  != substr(D2.Word, 3, 1)
+AND    substr(D1.Word, 11, 1) != substr(D2.Word, 3, 1) 
+/
+SELECT * FROM QUAGMIRE_DIAGRAPH;
+SELECT * FROM QUAGMIRE_DICTIONARY;
+
+SELECT   Word_Id, Count(*)
+FROM     QUAGMIRE_DICTIONARY_WORD
+WHERE    Valid = 'Y'
+GROUP BY Word_Id
+ORDER BY Word_Id
+;
+
+CREATE UNIQUE INDEX IDX_QD_DICT_ID  ON QUAGMIRE_DICTIONARY      (DICTIONARY_ID);
+CREATE  INDEX IDX_QDW_WORD_ID ON QUAGMIRE_DICTIONARY_WORD (WORD_ID);
+CREATE  INDEX IDX_QDW_DICT_ID ON QUAGMIRE_DICTIONARY_WORD (DICTIONARY_ID);
+
+SELECT Count(Distinct Solution_Id) FROM QUAGMIRE_SOLUTION ;
+
+
+        SELECT *
+        FROM  (SELECT *
+               FROM   V_QUAGMIRE_COMMON_OFFSET
+               WHERE  Word_Id1 NOT IN (SELECT Word_Id FROM QUAGMIRE_DIAGNOSTIC_RUN)
+               AND    Word_Id2     IN (SELECT Word_Id FROM QUAGMIRE_DIAGNOSTIC_RUN WHERE Solved = 'Y')
+               ORDER By MATCHED_LETTERS Desc, MISMATCHED_LETTERS Desc)
+        WHERE  Rownum = 1;
 
 
