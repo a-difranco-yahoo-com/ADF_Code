8ebf0cce6510dc3486d5f7629d944922916cb24b
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-04-19 20:10:48 +0100

    Update requests.php
    Removed $show and set $data->header directly
    Sinplified setting of $data->table
    Moved $Smarty setup to 1 place
    Change assignByRef => assign
    n oci_fetch_all changed OCI_ASSOC => OCI_NUM

--- WWW/Carnaby/requests.php
+++ WWW/Carnaby/requests.php
@@ -17,40 +17,34 @@ $data       = new Data;
 $data->from = $_POST['calendarDate1'];
 $data->to   = $_POST['calendarDate2'];
 $data->type = ($_POST['type']) ? $_POST['type'] : "inv_req";
-$smarty     = new Smarty;
-$smarty->assignByRef('data',$data);
 
 function List_Investors($conn, $data, $SiteSource)
 {
 	$sql = " SELECT C.Title, C.First_Name, C.Last_Name, C.Address_1, C.Country, C.Postcode, C.Item_Code "
 		 . " FROM   CONTACT      C "
          . " WHERE  C.Site_Source  = '" . $SiteSource . "' "
-	     . " AND    C.Date_Requested BETWEEN "
-	     . "           to_date('" . $data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
-	     . "      AND  to_date('" . $data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
+	     . " AND    trunc(C.Date_Requested) BETWEEN "
+	     . "        trunc( to_date('" . $data->from . "', 'DD-MM-YY') "
+	     . "    AND trunc( to_date('" . $data->to   . "', 'DD-MM-YY') ";
 
 	$stmt = ociparse($conn, $sql);
 	ociexecute($stmt);
-	oci_fetch_all($stmt, $rows, 0, 500, OCI_FETCHSTATEMENT_BY_ROW + OCI_RETURN_NULLS + OCI_ASSOC);
+	oci_fetch_all($stmt, $rows, 0, 500, OCI_FETCHSTATEMENT_BY_ROW + OCI_RETURN_NULLS + OCI_NUM);
 	return $rows;
 }
 
 switch ($_POST['type']) {
 case "inv_req":
     $rows = List_Investors($conn, $data, 'Investor Request');
-	$show = array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
 	break;	
 case "enq_req":
     $rows = List_Investors($conn, $data, 'Enquirer Request');
-	$show=array('TITLE','FIRST_NAME','ADDRESS_1','COUNTRY','LAST_NAME','POSTCODE','ITEM_CODE');
 	break;	
 case "med_req":
     $rows = List_Investors($conn, $data, 'Media Request');
-	$show=array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
 	break;
 case "inv_ch":
     $rows = List_Investors($conn, $data, 'Investor Channel');
-	$show=array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
 	break;
 default:
 	$data->from = date('d-m-Y',time()-7*24*3600); //week ago
@@ -72,17 +66,12 @@ if ($_POST['enter']=='File') {
 }
 
 if ($rows) {
-	$data->header = $show;
-	foreach ($rows as $i=>$row)
-		foreach ($show as $j=>$col) {
-			$data->table[$i][]=$row[$col];
-		}
-	foreach ($data->table as $i=>$row)
-		foreach ($row as $j=>$rec)
-			if (!$rec)
-				$data->table[$i][$j]="&nbsp;";
+	$data->header = array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
+	$data->table  = $rows;
 }
 ocilogoff($conn);
+$smarty = new Smarty;
+$smarty->assign('data', $data);
 $smarty->display('requests.tpl');
 
 ?>
