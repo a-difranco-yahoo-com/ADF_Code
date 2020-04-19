602197f31d0e39bb70a1e8d4c5b0f58f1f9c696c
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-04-19 13:41:28 +0100

    Update requests.php
    Streamline code to do standard SELECT
    Which then could be put into a function
    Use OCI_FETCH_ALL

--- WWW/Carnaby/requests.php
+++ WWW/Carnaby/requests.php
@@ -20,120 +20,36 @@ $data->type = ($_POST['type']) ? $_POST['type'] : "inv_req";
 $smarty     = new Smarty;
 $smarty->assignByRef('data',$data);
 
+function List_Investors($conn, $data, $SiteSource)
+{
+	$sql = " SELECT C.Title, C.First_Name, C.Last_Name, C.Address_1, C.Country, C.Postcode, C.Item_Code "
+		 . " FROM   CONTACT      C "
+         . " WHERE  C.Site_Source  = '" . $SiteSource . "' "
+	     . " AND    C.Date_Requested BETWEEN "
+	     . "           to_date('" . $data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
+	     . "      AND  to_date('" . $data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
+
+	$stmt = ociparse($conn, $sql);
+	ociexecute($stmt);
+	oci_fetch_all($stmt, $rows, 0, 500, OCI_FETCHSTATEMENT_BY_ROW + OCI_RETURN_NULLS + OCI_ASSOC);
+	return $rows;
+}
+
 switch ($_POST['type']) {
 case "inv_req":
-	$sql = " SELECT DISTINCT C.Person_Id, "
-	     . "        C.Title || ' ' || C.First_Name || ' ' || C.Last_Name INVESTOR_NAME, "
-         . "        C.Address_1, C.Address_2, C.Address_3, C.Address_4, C.Address_5, C.Address_6, "
-		 . "        C.Country,   C.Postcode,   C.Item_Code "
-		 . " FROM   CONTACT      C "
-         . " WHERE  C.Site_Source  = 'Investor Request' "
-	     . " AND    C.Date_Requested BETWEEN "
-	     . "           to_date('".$data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
-	     . "      AND  to_date('".$data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
-
-	$stmt = ociparse($conn,$sql);
-	ociexecute($stmt);
-	$oldid = -1;
-	$row_no = 0;
-	while (ocifetchinto($stmt, $row, OCI_RETURN_NULLS + OCI_ASSOC))
-	{
-		$id = array_shift($row);
-		if ($id == $oldid) {
-			$rows[$row_no-1]['ITEM_CODE'].="; ".$row['ITEM_CODE'];
-		} else {
-			$oldid=$id;
-			$rows[]=$row;
-			$row_no++;
-		}
-	}
-	$show=array('INVESTOR_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
-	break;
-	
+    $rows = List_Investors($conn, $data, 'Investor Request');
+	$show = array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
+	break;	
 case "enq_req":
-	$sql = " SELECT DISTINCT C.Person_Id, "
-	     . "        C.Title || ' ' || C.First_Name || ' ' || C.Last_Name INVESTOR_NAME, "
-         . "        C.Address_1, C.Address_2, C.Address_3, C.Address_4, C.Address_5, C.Address_6, "
-		 . "        C.Country,   C.Postcode,   C.Item_Code "
-		 . " FROM   CONTACT      C "
-         . " WHERE  C.Site_Source  = 'Enquirer Request' "
-	     . " AND    C.Date_Requested BETWEEN "
-	     . "           to_date('".$data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
-	     . "      AND  to_date('".$data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
-
-	$stmt = ociparse($conn,$sql);
-	ociexecute($stmt);
-	$oldid = -1;
-	$row_no = 0;
-	while (ocifetchinto($stmt, $row, OCI_RETURN_NULLS + OCI_ASSOC))
-	{
-		$id = array_shift($row);
-		if ($id == $oldid) {
-			$rows[$row_no-1]['ITEM_CODE'].="; ".$row['ITEM_CODE'];
-		} else {
-			$oldid=$id;
-			$rows[]=$row;
-			$row_no++;
-		}
-	}
-	$show=array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
-	break;
-	
+    $rows = List_Investors($conn, $data, 'Enquirer Request');
+	$show=array('TITLE','FIRST_NAME','ADDRESS_1','COUNTRY','LAST_NAME','POSTCODE','ITEM_CODE');
+	break;	
 case "med_req":
-	$sql = " SELECT DISTINCT C.Person_Id, "
-	     . "        C.Title || ' ' || C.First_Name || ' ' || C.Last_Name INVESTOR_NAME, "
-         . "        C.Address_1, C.Address_2, C.Address_3, C.Address_4, C.Address_5, C.Address_6, "
-		 . "        C.Country,   C.Postcode,   C.Item_Code "
-		 . " FROM   CONTACT      C "
-         . " WHERE  C.Site_Source  = 'Media Request' "
-	     . " AND    C.Date_Requested BETWEEN "
-	     . "           to_date('".$data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
-	     . "      AND  to_date('".$data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
-
-	//echo $sql;
-	$stmt = ociparse($conn,$sql);
-	ociexecute($stmt);
-	$oldid = -1;
-	$row_no = 0;
-	while (ocifetchinto($stmt, $row, OCI_RETURN_NULLS + OCI_ASSOC))
-	{
-		$id = array_shift($row);
-		if ($id == $oldid) {
-			$rows[$row_no-1]['ITEM_CODE'].="; ".$row['ITEM_CODE'];
-		} else {
-			$oldid=$id;
-			$rows[]=$row;
-			$row_no++;
-		}
-	}
+    $rows = List_Investors($conn, $data, 'Media Request');
 	$show=array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
 	break;
 case "inv_ch":
-	$sql = " SELECT DISTINCT C.Person_Id, "
-	     . "        C.Title || ' ' || C.First_Name || ' ' || C.Last_Name INVESTOR_NAME, "
-         . "        C.Address_1, C.Address_2, C.Address_3, C.Address_4, C.Address_5, C.Address_6, "
-		 . "        C.Country,   C.Postcode,   C.Item_Code "
-		 . " FROM   CONTACT      C "
-         . " WHERE  C.Site_Source  = 'Investor Channel' "
-	     . " AND    C.Date_Requested BETWEEN "
-	     . "           to_date('".$data->from . " 00:00:00','DD-MM-YY HH24:MI:SS') "
-	     . "      AND  to_date('".$data->to   . " 23:59:59','DD-MM-YY HH24:MI:SS') ";
-
-	$stmt = ociparse($conn,$sql);
-	ociexecute($stmt);
-	$oldid = -1;
-	$row_no = 0;
-	while (ocifetchinto($stmt, $row, OCI_RETURN_NULLS + OCI_ASSOC))
-	{
-		$id = array_shift($row);
-		if ($id == $oldid) {
-			$rows[$row_no-1]['ITEM_CODE'].="; ".$row['ITEM_CODE'];
-		} else {
-			$oldid=$id;
-			$rows[]=$row;
-			$row_no++;
-		}
-	}
+    $rows = List_Investors($conn, $data, 'Investor Channel');
 	$show=array('TITLE','FIRST_NAME','LAST_NAME','ADDRESS_1','COUNTRY','POSTCODE','ITEM_CODE');
 	break;
 default:
@@ -143,7 +59,6 @@ default:
 }
 
 if ($_POST['enter']=='File') {
-	//header('Content-Type: text/csv; charset=ISO-8859-1');
 	header('Content-Type: application/vnd.ms-excel');
 	header('Content-Disposition: attachment; filename="carnaby.csv"');
 	if ($rows && sizeof($rows)) {
