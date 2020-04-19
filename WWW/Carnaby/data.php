b161e8078ea7e3d08a02066e968af56e0aa91c3a
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-04-19 13:42:04 +0100

    Update data.php
    Removed a set of columns

--- WWW/Carnaby/data.php
+++ WWW/Carnaby/data.php
@@ -39,30 +39,14 @@ $form2 = array(
   array("Telephone Evening",       "TELEPHONE_EVENING",      40),
   array("Telephone Mobile",        "TELEPHONE_MOBILE",       40),
   array("Status",                  "STATUS",                 "radio",  array('A'=>'A','D'=>'D','S'=>'S','L'=>'L')),
-  array("MPS",                     "DO_NOT_MAIL",            "radio",  array('Y'=>'yes','N'=>'no')),
-  array("TPS",                     "DO_NOT_TELEPHONE",       "radio",  array('Y'=>'yes','N'=>'no')),
-  array("Do not email",            "DO_NOT_EMAIL",           "radio",  array('Y'=>'yes','N'=>'no')),
-  array("Sales Status",            "SALES_STATUS",           "select", array(""=>"", "Allocated"=>"Allocated", "Existing"=>"Existing","Investor"=>"Investor","KOL"=>"KOL","New"=>"New","Qualified"=>"Qualified")),
-  array("Created by",              "CREATED_BY",              20),
-  array("Created",                 "CREATED",                 11, $date),
-  array("Last changed",            "LAST_CHANGED",            11, $date),
-  array("Last changed by",         "LAST_CHANGED_BY",         20),
-  array("Geodemographic Category", "GEODEMOGRAPHIC_CATEGORY", 40),
-  array("Segmentation Category",   "SEGMANTATION_CATEGORY",   40),
-  array("Age Band",                "AGE_BAND",                40),
-  array("MID Identifier",          "MID_IDENTIFIER",          5)
 );
 
 $form3 = array(
-  array("GM Account No", "GM_ACCOUNTNO",  30),
-  array("Salesman",      "SALESMAN",      20),
   array("Pref Comedy",   "PREF_COMEDY",   "radio", array('Y'=>'yes','N'=>'no')),
   array("Pref Drama",    "PREF_DRAMA",    "radio", array('Y'=>'yes','N'=>'no')),
   array("Pref Horror",   "PREF_HORROR",   "radio", array('Y'=>'yes','N'=>'no')),
   array("Pref Romance",  "PREF_ROMANCE",  "radio", array('Y'=>'yes','N'=>'no')),
   array("Pref SciFi",    "PREF_SCIFI",    "radio", array('Y'=>'yes','N'=>'no')),
-  array("Pref Thriller", "PREF_THRILLER", "radio", array('Y'=>'yes','N'=>'no')),
-  array("Pref Other",    "PREF_OTHER",    500),
-  array("From Kahoona",  "FROM_KAHOONA",  "radio", array('Y'=>'yes','N'=>'no')),
+  array("Pref Thriller", "PREF_THRILLER", "radio", array('Y'=>'yes','N'=>'no'))
 );
 ?>
