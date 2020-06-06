56531f091e3c7339e2abb083d693480293560133
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-06-06 12:41:54 +0100

    Update FreeCodeCamp_Draw.php
    Include Visual Design section

--- Free_Code_Camp/FreeCodeCamp_Draw.php
+++ Free_Code_Camp/FreeCodeCamp_Draw.php
@@ -3,11 +3,13 @@
 require('D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\FreeCodeCamp_PDF.php');
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_HTML_Section.php';
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_CSS_Section.php';
+include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Visual_Design_Section.php';
 
 
    $pdf = new FREECODECAMP_PDF();
    GenerateHTMLManual($pdf);
    GenerateCSSManual($pdf);
+   GenerateVisualDesignManual($pdf);
    $pdf->Output("Free_Code_Camp.pdf", "F");
 
 ?>
