2584316dee3c69503e40a8b897cc81bdb6871370
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-06-25 14:29:42 +0100

    Update FreeCodeCamp_Draw.php
    Add Applied Accessibility section

--- Free_Code_Camp/FreeCodeCamp_Draw.php
+++ Free_Code_Camp/FreeCodeCamp_Draw.php
@@ -4,12 +4,14 @@ require('D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\FreeCodeCamp_PDF.php')
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_HTML_Section.php';
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_CSS_Section.php';
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Visual_Design_Section.php';
+include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Applied_Access_Section.php';
 
 
    $pdf = new FREECODECAMP_PDF();
    GenerateHTMLManual($pdf);
    GenerateCSSManual($pdf);
    GenerateVisualDesignManual($pdf);
+   GenerateAppliedAccessManual($pdf);
    $pdf->Output("Free_Code_Camp.pdf", "F");
 
 ?>
