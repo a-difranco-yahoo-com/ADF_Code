8c13d9a91541a1b4f406e283506ab32ab0c04180
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-07-08 12:10:20 +0100

    Update FreeCodeCamp_Draw.php
    included Responsive Web Design section

--- Free_Code_Camp/FreeCodeCamp_Draw.php
+++ Free_Code_Camp/FreeCodeCamp_Draw.php
@@ -5,6 +5,7 @@ include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_HTML_Section.php'
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_CSS_Section.php';
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Visual_Design_Section.php';
 include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Applied_Access_Section.php';
+include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Responsive_Web_Design_Section.php';
 
 
    $pdf = new FREECODECAMP_PDF();
@@ -12,6 +13,7 @@ include 'D:\Php_Code\GitHub_Local\ADF_Code\Free_Code_Camp\Draw_Applied_Access_Se
    GenerateCSSManual($pdf);
    GenerateVisualDesignManual($pdf);
    GenerateAppliedAccessManual($pdf);
+   GenerateResponsiveDesignManual($pdf);
    $pdf->Output("Free_Code_Camp.pdf", "F");
 
 ?>
