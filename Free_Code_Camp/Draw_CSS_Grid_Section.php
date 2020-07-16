17453f23c7c64cc46b455264e828d267e21f1126
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-07-16 14:33:49 +0100

    Update Draw_CSS_Grid_Section.php
    Added lessons 3 - 6

--- Free_Code_Camp/Draw_CSS_Grid_Section.php
+++ Free_Code_Camp/Draw_CSS_Grid_Section.php
@@ -72,12 +72,162 @@
 	   $pdf->AddLessonCode("</div>");
    }
  
+   function GenerateGrLesson3($pdf) {
+       $pdf->AddLessonTitle("Add Rows with grid-template-rows");
+	   $pdf->AddLessonText("The grid you created in the last challenge will set the number of rows automatically. To adjust the rows manually, use the grid-template-rows property in the same way you used grid-template-columns in previous challenge.");
+	   $pdf->AddLessonText("Add two rows to the grid that are 50px tall each.");
 
+	   $pdf->DrawCodeArea("24");
+	   $pdf->AddLessonCode("<style>");
+	   $pdf->AddLessonCode("  .d1{background:LightSkyBlue;}");
+	   $pdf->AddLessonCode("  .d2{background:LightSalmon;}");
+	   $pdf->AddLessonCode("  .d3{background:PaleTurquoise;}");
+	   $pdf->AddLessonCode("  .d4{background:LightPink;}");
+	   $pdf->AddLessonCode("  .d5{background:PaleGreen;}");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("  .container {");
+	   $pdf->AddLessonCode("    font-size: 40px;");
+	   $pdf->AddLessonCode("    width: 100%;");
+	   $pdf->AddLessonCode("    background: LightGray;");
+	   $pdf->AddLessonCode("    display: grid;");
+	   $pdf->AddLessonCode("    grid-template-columns: 100px 100px 100px;");
+	   $pdf->AddLessonCode("    grid-template-rows:    50px  50px;");
+	   $pdf->AddLessonCode("  }");
+	   $pdf->AddLessonCode("</style>");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("<div class='container'>");
+	   $pdf->AddLessonCode("  <div class='d1'>1</div>");
+	   $pdf->AddLessonCode("  <div class='d2'>2</div>");
+	   $pdf->AddLessonCode("  <div class='d3'>3</div>");
+	   $pdf->AddLessonCode("  <div class='d4'>4</div>");
+	   $pdf->AddLessonCode("  <div class='d5'>5</div>");
+	   $pdf->AddLessonCode("</div>");
+   }
+ 
+   function GenerateGrLesson4($pdf) {
+       $pdf->AddLessonTitle("Use CSS Grid units to Change the Size of Columns and Rows");
+	   $pdf->AddLessonText("You can use absolute and relative units like px and em in CSS Grid to define the size of rows and columns. You can use these as well:");
+	   $pdf->AddLessonText("fr: sets the column or row to a fraction of the available space,");
+	   $pdf->AddLessonText("auto: sets the column or row to the width or height of its content automatically,");
+	   $pdf->AddLessonText("%: adjusts the column or row to the percent width of its container.");
+	   $pdf->AddLessonText("Here's the code that generates the output in the preview:");
 
+	   $pdf->DrawCodeArea("1");
+	   $pdf->AddLessonCode("grid-template-columns: auto 50px 10% 2fr 1fr;");
+
+	   $pdf->AddLessonText("");
+	   $pdf->AddLessonText("This snippet creates five columns. The first column is as wide as its content, the second column is 50px, the third column is 10% of its container, and for the last two columns; the remaining space is divided into three sections, two are allocated for the fourth column, and one for the fifth.");
+	   $pdf->AddLessonText("Make a grid with three columns whose widths are as follows: 1fr, 100px, and 2fr.");
+
+	   $pdf->DrawCodeArea("24");
+	   $pdf->AddLessonCode("<style>");
+	   $pdf->AddLessonCode("  .d1{background:LightSkyBlue;}");
+	   $pdf->AddLessonCode("  .d2{background:LightSalmon;}");
+	   $pdf->AddLessonCode("  .d3{background:PaleTurquoise;}");
+	   $pdf->AddLessonCode("  .d4{background:LightPink;}");
+	   $pdf->AddLessonCode("  .d5{background:PaleGreen;}");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("  .container {");
+	   $pdf->AddLessonCode("    font-size: 40px;");
+	   $pdf->AddLessonCode("    width: 100%;");
+	   $pdf->AddLessonCode("    background: LightGray;");
+	   $pdf->AddLessonCode("    display: grid;");
+	   $pdf->AddLessonCode("    grid-template-columns: 1fr 100px 2fr;");
+	   $pdf->AddLessonCode("    grid-template-rows:    50px  50px;");
+	   $pdf->AddLessonCode("  }");
+	   $pdf->AddLessonCode("</style>");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("<div class='container'>");
+	   $pdf->AddLessonCode("  <div class='d1'>1</div>");
+	   $pdf->AddLessonCode("  <div class='d2'>2</div>");
+	   $pdf->AddLessonCode("  <div class='d3'>3</div>");
+	   $pdf->AddLessonCode("  <div class='d4'>4</div>");
+	   $pdf->AddLessonCode("  <div class='d5'>5</div>");
+	   $pdf->AddLessonCode("</div>");
+   }
+ 
+   function GenerateGrLesson5($pdf) {
+       $pdf->AddLessonTitle("Create a Column Gap Using grid-column-gap");
+	   $pdf->AddLessonText("So far in the grids you have created, the columns have all been tight up against each other. Sometimes you want a gap in between the columns. To add a gap between the columns, use the grid-column-gap property like this:");
+
+	   $pdf->DrawCodeArea("1");
+	   $pdf->AddLessonCode("grid-column-gap: 10px;");
+
+	   $pdf->AddLessonText("");
+	   $pdf->AddLessonText("This creates 10px of empty space between all of our columns.");
+	   $pdf->AddLessonText("Give the columns in the grid a 20px gap.");
+
+	   $pdf->DrawCodeArea("26");
+	   $pdf->AddLessonCode("<style>");
+	   $pdf->AddLessonCode("  .d1{background:LightSkyBlue;}");
+	   $pdf->AddLessonCode("  .d2{background:LightSalmon;}");
+	   $pdf->AddLessonCode("  .d3{background:PaleTurquoise;}");
+	   $pdf->AddLessonCode("  .d4{background:LightPink;}");
+	   $pdf->AddLessonCode("  .d5{background:PaleGreen;}");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("  .container {");
+	   $pdf->AddLessonCode("    font-size: 40px;");
+	   $pdf->AddLessonCode("    min-height: 300px;");
+	   $pdf->AddLessonCode("    width: 100%;");
+	   $pdf->AddLessonCode("    background: LightGray;");
+	   $pdf->AddLessonCode("    display: grid;");
+	   $pdf->AddLessonCode("    grid-template-columns: 1fr 1fr 1fr;");
+	   $pdf->AddLessonCode("    grid-template-rows:    1fr 1fr 1fr;");
+	   $pdf->AddLessonCode("    grid-column-gap:       20px;");
+	   $pdf->AddLessonCode("  }");
+	   $pdf->AddLessonCode("</style>");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("<div class='container'>");
+	   $pdf->AddLessonCode("  <div class='d1'>1</div>");
+	   $pdf->AddLessonCode("  <div class='d2'>2</div>");
+	   $pdf->AddLessonCode("  <div class='d3'>3</div>");
+	   $pdf->AddLessonCode("  <div class='d4'>4</div>");
+	   $pdf->AddLessonCode("  <div class='d5'>5</div>");
+	   $pdf->AddLessonCode("</div>");
+   }
+ 
+   function GenerateGrLesson6($pdf) {
+       $pdf->AddLessonTitle("Create a Row Gap using grid-row-gap");
+	   $pdf->AddLessonText("You can add a gap in between the rows of a grid using grid-row-gap in the same way that you added a gap in between columns in the previous challenge.");
+	   $pdf->AddLessonText("Create a gap for the rows that is 5px tall.");
+
+	   $pdf->DrawCodeArea("26");
+	   $pdf->AddLessonCode("<style>");
+	   $pdf->AddLessonCode("  .d1{background:LightSkyBlue;}");
+	   $pdf->AddLessonCode("  .d2{background:LightSalmon;}");
+	   $pdf->AddLessonCode("  .d3{background:PaleTurquoise;}");
+	   $pdf->AddLessonCode("  .d4{background:LightPink;}");
+	   $pdf->AddLessonCode("  .d5{background:PaleGreen;}");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("  .container {");
+	   $pdf->AddLessonCode("    font-size: 40px;");
+	   $pdf->AddLessonCode("    min-height: 300px;");
+	   $pdf->AddLessonCode("    width: 100%;");
+	   $pdf->AddLessonCode("    background: LightGray;");
+	   $pdf->AddLessonCode("    display: grid;");
+	   $pdf->AddLessonCode("    grid-template-columns: 1fr 1fr 1fr;");
+	   $pdf->AddLessonCode("    grid-template-rows:    1fr 1fr 1fr;");
+	   $pdf->AddLessonCode("    grid-row-gap:          5px;");
+	   $pdf->AddLessonCode("  }");
+	   $pdf->AddLessonCode("</style>");
+	   $pdf->AddLessonCode("");
+	   $pdf->AddLessonCode("<div class='container'>");
+	   $pdf->AddLessonCode("  <div class='d1'>1</div>");
+	   $pdf->AddLessonCode("  <div class='d2'>2</div>");
+	   $pdf->AddLessonCode("  <div class='d3'>3</div>");
+	   $pdf->AddLessonCode("  <div class='d4'>4</div>");
+	   $pdf->AddLessonCode("  <div class='d5'>5</div>");
+	   $pdf->AddLessonCode("</div>");
+   }
+ 
  function GenerateCSSGridManual($pdf) {
 	   $pdf->AddSubject("Introduction to the CSS Grid Challenges");
 	   GenerateGrLesson1($pdf);
 	   GenerateGrLesson2($pdf);
+	   GenerateGrLesson3($pdf);
+	   GenerateGrLesson4($pdf);
+	   GenerateGrLesson5($pdf);
+	   GenerateGrLesson6($pdf);
    }
 
 ?>
