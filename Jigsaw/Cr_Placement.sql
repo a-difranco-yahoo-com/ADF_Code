c0990b26b282edec0de64533d556fb2cb28a8a07
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-07-25 10:30:12 +0100

    Moved to own repo

--- Jigsaw/Cr_Placement.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE PLACEMENT (
-Placement_Id    NUMBER      NOT NULL,
-Shape_Id        NUMBER      NOT NULL,
-Square_Id       NUMBER      NOT NULL,
-Letter          VARCHAR2(1) NOT NULL)
-/
--- Jigsaw/Cr_Ref_Square.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE REF_SQUARE (
-Square_Id        NUMBER      NOT NULL,
-Row_Number       NUMBER      NOT NULL,
-Column_Number    NUMBER      NOT NULL)
-/
