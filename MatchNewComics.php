5cb58416267b6482232498bb132c39063db24d1c
a-difranco-yahoo-com <a.difranco@yahoo.com>
2020-08-29 16:32:34 +0100

    Handle new POST variables for NEW_DIGITAL_COMIC statuses

--- MatchNewComics.php
+++ MatchNewComics.php
@@ -4,11 +4,15 @@ require_once("D:\Php_Code\Smarty\libs\Smarty.class.php");
 include 'Comic_Oracle.php';
 
 $err=error_reporting(E_ALL & ~E_NOTICE);
-$Connection = new Comic_Oracle();
-$Option     =$_POST['Option'];
-$Commit     =$_POST['Commit'];
-$PullMatch  =$_POST['MatchToPull'];
-$smarty     = new Smarty;
+$Connection    = new Comic_Oracle();
+$Option        =$_POST['Option'];
+$Commit        =$_POST['Commit'];
+$PullMatch     =$_POST['MatchToPull'];
+$DigitalSeries =$_POST['DigitalSeries'];
+$OneShot       =$_POST['OneShot'];
+$NewSeries     =$_POST['NewSeries'];
+$BackIssue     =$_POST['BackIssue'];
+$smarty        = new Smarty;
 
 $Connection->Log_Post_Details('POST', $_POST);
 $Connection->Log_Post_Details('GET',  $_GET);
@@ -16,9 +20,6 @@ $Connection->Log_Post_Details('GET',  $_GET);
   if ($Commit == 'CommitMatchWish')  {
 	  $Connection->Commit_Match_Wish_List($_POST);
 	$Option = 'MatchWish';
-  } elseif ($Commit == 'CommitNew')  {
-	  $Connection->Commit_New_Comics($_POST);
-	  $Option = 'ViewNew';
   } elseif ($Commit == 'CommitPull')  {
 	  $Connection->Commit_Pull_List($_POST);
 	  $Option = 'ViewPull';
@@ -27,6 +28,18 @@ $Connection->Log_Post_Details('GET',  $_GET);
   if ($PullMatch != "") {
 	  $Connection->Commit_Match_To_Pull_List($PullMatch);
     $Option      = "MatchPull";
+  } elseif ($DigitalSeries != '') {
+	  $Connection->Clear_New_Digital_Comic($DigitalSeries);
+    $Option = 'ViewNew';
+  } elseif ($OneShot != '') {
+	  $Connection->Clear_New_Digital_Comic($OneShot);
+    $Option = 'ViewNew';
+  } elseif ($NewSeries != '') {
+	  $Connection->Clear_New_Digital_Comic($NewSeries);
+    $Option = 'ViewNew';
+  } elseif ($BackIssue != '') {
+	  $Connection->Clear_New_Digital_Comic($BackIssue);
+    $Option = 'ViewNew';
   } elseif ($Option == '') {
     $Option = 'MatchPull';
   }
